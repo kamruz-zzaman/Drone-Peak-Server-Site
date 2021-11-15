3b4688a88d21efbcad301129c93c97f1c45c7115
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-11-15 22:55:32 +0600

    all done

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ async function run() {
         const database = client.db("DronePeak");
         const packagesCollection = database.collection("Drone");
         const orderCollection = database.collection("Order");
-        const ratingCollection = database.collection("Rating");
+        const reviewCollection = database.collection("Review");
+        const usersCollection = database.collection("User");
 
         // get all the drones packages
         app.get('/drones', async (req, res) => {
@@ -49,18 +50,95 @@ async function run() {
             res.send(order);
         });
 
+        // get order by email
+        app.get('/order', async (req, res) => {
+            const email = req.query.Email;
+            const query = { email: email }
+            const cursor = orderCollection.find(query);
+            const order = await cursor.toArray();
+            res.json(order);
+        })
+
         // get orderInfo 
         app.get('/order', async (req, res) => {
             const cursor = orderCollection.find({});
             const order = await cursor.toArray();
             res.send(order);
         });
-        // updload rating
-        app.post('/rating', async (req, res) => {
-            const data = req.body;
-            const rating = await ratingCollection.insertOne(data);
-            res.send(rating);
+        // manage order
+        app.put('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    Status: `Shipped`
+                },
+            };
+            const result = orderCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+            res.send(result);
         });
+        // delet on order info
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const orderr = await orderCollection.deleteOne(query);
+            res.send(orderr);
+        })
+        // updload review
+        app.post('/review', async (req, res) => {
+            const data = req.body;
+            const review = await reviewCollection.insertOne(data);
+            res.send(review);
+        });
+
+        // get all review
+        app.get('/review', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+
+        // add user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        // update user
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // make admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        // search admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
     } finally {
         // await client.close();
     };
